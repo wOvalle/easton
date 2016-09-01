9037fa6f05e31177e09e2a1cf9a3fd020ee072e0
Willy Ovalle <willyovalle16@gmail.com>
2016-09-01 00:55:12 -0400

    Feature: Adding arduino handler

---
+++ arduino/arduino.js
@@ -0,0 +1,35 @@
+const five = require("johnny-five")
+const messenger = require('messenger')
+client = messenger.createSpeaker(8000)
+
+let board, buttons
+
+board = new five.Board()
+
+const buttonsObj = {
+    15: "A1",
+    16: "A2",
+    17: "A3",
+    18: "A4",
+    19: "A5",
+    20: "A6",
+    21: "A7"
+}
+
+const getVideoId = (button) => button.split('')[1]
+
+board.on("ready", function() {
+    var buttons = new five.Buttons({
+        pins: ['A1', 'A2', 'A3']
+    })
+
+    buttons.on("press", function(event) {
+        const pressedButton = buttonsObj[event.pin]
+        console.log("Pressed: ", pressedButton)
+
+        client.request('button-pressed', getVideoId(pressedButton), function(data) {
+            console.log('Event sent to frontend: ', data)
+        })
+    })
+
+})
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const {ipcMain, BrowserWindow, app} = require('electron')
+const { ipcMain, BrowserWindow, app } = require('electron')
 const menu = require('./menu')
+const messenger = require('messenger')
+const arduino = messenger.createListener(8000)
+let mainWindow = {}
+
 menu.applyCustomMenu()
 
 if (process.mas) app.setName('Arduino Video Changer')
 
 require('electron-reload')(__dirname + '/client')
 
-
-app.on('ready', function(){
-    var mainWindow = new BrowserWindow({
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         title: app.getName()
@@ -18,6 +21,11 @@ app.on('ready', function(){
 })
 
 ipcMain.on('change-video', (event, arg) => {
-  event.sender.send('change-video', arg)
+    event.sender.send('change-video', arg)
 })
 
+arduino.on('button-pressed', function(event, data) {
+    console.log('Received From Arduino: ', data)
+    event.reply('success')
+    mainWindow.webContents.send('change-video', data);
+})
